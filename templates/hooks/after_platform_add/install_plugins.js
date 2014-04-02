8afb245c8270cbf7ce728bfbdddaf1486d67cbe1
Guillermo Ferrer <guilleferrer@gmail.com>
2014-04-02 19:07:20 +0200

    Adds install_plugins hook after adding a platform

---
+++ templates/hooks/after_platform_add/install_plugins.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+//this hook installs all your plugins
+
+// add your plugins to this list--either the identifier, the filesystem location or the URL
+var pluginlist = [
+    'https://github.com/phonegap-build/GAPlugin',
+    'https://github.com/phonegap-build/PushPlugin',
+    'org.apache.cordova.console',
+    'org.apache.cordova.device',
+    'org.apache.cordova.dialogs',
+    'org.apache.cordova.inappbrowser',
+    'org.apache.cordova.splashscreen'
+];
+
+// no need to configure below
+
+var fs = require('fs');
+var path = require('path');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) {
+    sys.puts(stdout)
+}
+
+pluginlist.forEach(function (plug) {
+    exec("cordova plugin add " + plug, puts);
+});
